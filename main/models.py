e3ac344398d2eb701f66264105a1c19012bab605
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-08-30 17:00:42 +0530

    created superuser and db

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+class customer(models.Model):
+    email=models.EmailField()
+    name=models.TextField(max_length=20)
+    address=models.TextField(max_length=20)
+    address2=models.TextField(max_length=20)
+    city=models.TextField(max_length=20)
+    state=models.TextField(max_length=20)
+    password=models.TextField(max_length=20)
